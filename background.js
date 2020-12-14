1d2ab40057853bbe33ac9bd03a334484d1f0274b
noodlez04 <noodlez040@gmail.com>
2020-12-15 01:08:18 +0200

    Added support for daily balance extraction

--- background.js
+++ background.js
@@ -1,8 +1,47 @@
 const AUTOBIS_SCHEDULE_ALARM_NAME = 'AutobisSchedule'
 
+async function delay(milliseconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, milliseconds);
+  });
+}
+
+// taken from https://stackoverflow.com/a/44864966/5259379
+async function createTab (url) {
+  return new Promise(resolve => {
+      chrome.tabs.create({
+        url,
+        active: true
+      }, async tab => {
+          chrome.tabs.onUpdated.addListener(function listener (tabId, info) {
+              if (info.status === 'complete' && tabId === tab.id) {
+                  chrome.tabs.onUpdated.removeListener(listener);
+                  resolve(tab);
+              }
+          });
+      });
+  });
+}
+
+async function executeScriptWithPromise(tabId, details) {
+  return new Promise(resolve => {
+    chrome.tabs.executeScript(tabId, details, async result => {
+      resolve(result);
+    });
+  })
+}
+
+async function getDailyBalance() {
+  let tab = await createTab('https://www.10bis.co.il/next/user-report');
+  let balance = await executeScriptWithPromise(tab.id, {file: "get_daily_balance.js"});
+  balance = balance[0];
+  console.log(balance);
+  chrome.tabs.remove(tab.id);
+}
+
 function createAutobisSchedule() {
   var now = new Date();
-  var timestamp = +new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 30, 0, 0);
+  var timestamp = +new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 18, 0, 0);
 
   chrome.alarms.create(AUTOBIS_SCHEDULE_ALARM_NAME, {
       when: timestamp,
@@ -14,6 +53,7 @@ function createAutobisSchedule() {
 chrome.alarms.onAlarm.addListener(alarm => {
   if (alarm.name === AUTOBIS_SCHEDULE_ALARM_NAME) {
       console.log(new Date());
+      getDailyBalance()
   }
 });
 
---
+++ get_daily_balance.js
@@ -0,0 +1,7 @@
+let balanceElement = document.querySelector('.MoneyCardDetail__DetailsBox-q2rq3n-0.ffQfNF > div.MoneyCardDetail__Sum-q2rq3n-1.hqiiyz:nth-of-type(3)');
+let balance = balanceElement?.innerText.match(/\d+/)[0]
+if (balance) {
+  balance = parseFloat(balance);
+}
+
+balance;
